6fbde3fffad489b43290cbc74f273a6157e72a95
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2023-06-27 20:42:18 -0300

    altercoes do joao

--- src/app/components/anunciar/anunciar.component.ts
+++ src/app/components/anunciar/anunciar.component.ts
@@ -1,83 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AnunciarService } from './anunciar.service';
 import { LocalStorageService } from 'src/shared/services/local-storage.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-anunciar',
   templateUrl: './anunciar.component.html',
   styleUrls: ['./anunciar.component.css']
 })
-export class AnunciarComponent implements OnInit {
-  anunciarForm: FormGroup = Object.create(null);
-  
-  constructor(private anuncioS: AnunciarService, private user: LocalStorageService, private http: HttpClient) { }
-  
-  ngOnInit() {
-    this.createFormAnuncio();
-  }
+export class AnunciarComponent {
+  photoUploadForm: FormGroup;
+  selectedFile: any = null;
 
-  createFormAnuncio(): void {
-    this.anunciarForm = new FormGroup({
-      dsin: new FormControl('', [Validators.required]),
-      valor: new FormControl('', [Validators.required]),
-      foto: new FormControl('', [Validators.required]),
-      ddet: new FormControl(''),
-      qtd_banheiro: new FormControl('', [Validators.required]),
-      qtd_quarto: new FormControl('', [Validators.required]),
-      wifi: new FormControl(''),
-      ar: new FormControl(''),
-      manutencao: new FormControl(''),
-      limpeza: new FormControl(''),
-      fumantes: new FormControl('', [Validators.required]),
-      criancas: new FormControl('', [Validators.required]),
-      cep: new FormControl('', [Validators.required]),
-      logradouro: new FormControl('', [Validators.required]),
-      bairro: new FormControl('', [Validators.required]),
-      cidade: new FormControl('', [Validators.required]),
-      complemento: new FormControl(''),
-      referencia: new FormControl(''),
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.photoUploadForm = this.formBuilder.group({
+      description: '',
+      file: null
     });
   }
 
-  createAnuncio(){
-    if(this.anunciarForm.valid){    
-      let json = {
-        "dsin": this.anunciarForm.value.dsin,
-        "valor": this.anunciarForm.value.valor,
-        "ddet": this.anunciarForm.value.ddet,
-        "qtd_banheiro": this.anunciarForm.value.qtd_banheiro,
-        "qtd_quarto": this.anunciarForm.value.qtd_quarto,
-        "wifi": this.anunciarForm.value.wifi,
-        "ar": this.anunciarForm.value.ar,
-        "manutencao": this.anunciarForm.value.manutencao,
-        "limpeza": this.anunciarForm.value.limpeza,
-        "fumantes": this.anunciarForm.value.fumantes,
-        "criancas": this.anunciarForm.value.criancas,
-        "userID": 1,
-        "endereco": {
-          "cep": this.anunciarForm.value.cep,
-          "rua": this.anunciarForm.value.logradouro,
-          "bairro": this.anunciarForm.value.bairro,
-          "cidade": this.anunciarForm.value.cidade,
-          "pais": "Brasil",
-          "referencia": this.anunciarForm.value.referencia,
-          "complemento": this.anunciarForm.value.complemento
-        },
-        "locacaoID": "1"
-      }
-
-      const formData = new FormData();
-      formData.append('file', this.anunciarForm.value.foto);
-      formData.append('id', '1');
-
-      alert("deu bom");
-      this.anuncioS.createFoto(formData).subscribe({next: (res) => {console.log(res)}, error: (error) => {console.log(error)}})
-      // this.anuncioS.create(json).subscribe({next: (res) => {console.log(res)}, error: (error) => {console.log(error)}})
-    } else {
-      alert('deu ruim');
+  onSubmit() {console.log('aaaaaaaa')
+    const formData = new FormData();
+    if (this.selectedFile) {
+      formData.append('file', this.selectedFile);
     }
+    formData.append('description', this.photoUploadForm.value.description);
+
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+
+    this.http.post('http://localhost:8080/api/photos', formData, { headers: headers }).subscribe(
+      (res) => console.log(res),
+      (err) => console.error(err)
+    );
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
   }
 
 }

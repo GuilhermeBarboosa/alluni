ce76c3ea5a413b0a78e8f8c76f54ee9a78fb1205
Guilherme Barbosa <78804519+GuilhermeBarboosa@users.noreply.github.com>
2023-06-28 13:04:42 -0300

    Revert "feat filtro de busca por cidade"
    This reverts commit 9769f07d6c04b2c7f26f1cb120e2664cd692fb6d.

--- src/shared/filtro-busca/filtro-busca.component.ts
+++ src/shared/filtro-busca/filtro-busca.component.ts
@@ -1,6 +1,5 @@
 import {Component, Injectable, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
-import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {startWith, map} from 'rxjs/operators';
 
@@ -10,38 +9,39 @@ import {startWith, map} from 'rxjs/operators';
   styleUrls: ['./filtro-busca.component.css']
 })
 export class FiltroBuscaComponent implements OnInit {
-  opcoesAutocompletar1: string[] = ['Uberaba', 'Uberlândia', 'Contagem'];
-  // opcoesAutocompletar2: string[] = ['Opção A', 'Opção B', 'Opção C'];
-  campoAutocompletar1 = new FormControl();
-  campoAutocompletar2 = new FormControl();
+  formEstado = new FormControl('');
+  estado: string[] = ['Minas', 'São Paulo', 'Santa Catarina', 'Fifth Avenue'];
+  filtroEstado: Observable<string[]> | undefined;
+
+  formCidade = new FormControl('');
+  cidade: string[] = ['Teste', 'AAAA', 'Testando...', 'Fifth Avenue'];
+  filtroCidade: Observable<string[]> | undefined;
 
   ngOnInit() {
 
-  }
-
-  selecionarOpcao1(event: MatAutocompleteSelectedEvent): void {
-    console.log('Opção 1 selecionada:', event.option.value);
-  }
-
-  selecionarOpcao2(event: MatAutocompleteSelectedEvent): void {
-    console.log('Opção 2 selecionada:', event.option.value);
-  }
-
-  filtrarOpcoes1(valor: string): string[] {
-    if (!valor) {
-      return this.opcoesAutocompletar1;
-    }
-    return this.opcoesAutocompletar1.filter(opcao =>
-      opcao.toLowerCase().includes(valor.toLowerCase())
+    this.filtroCidade = this.formCidade.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '', 2)),
     );
+
+    this.filtroEstado = this.formEstado.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '', 1)),
+    );
+
   }
 
-  // filtrarOpcoes2(valor: string): string[] {
-  //   if (!valor) {
-  //     return this.opcoesAutocompletar2;
-  //   }
-  //   return this.opcoesAutocompletar2.filter(opcao =>
-  //     opcao.toLowerCase().includes(valor.toLowerCase())
-  //   );
-  // }
+  private _filter(value: string, tipo: number): string[] {
+    const filterValue = this._normalizeValue(value);
+
+    if(tipo == 1){
+      return this.estado.filter(estado => this._normalizeValue(estado).includes(filterValue));
+    }else{
+      return this.cidade.filter(cidade => this._normalizeValue(cidade).includes(filterValue));
+    }
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
 }
